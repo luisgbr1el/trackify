5505c259a747d6b3b38f59b9c2027fecee9ac9d6
luisgbr1el <harrycorpmail@gmail.com>
2023-04-21 16:08:19 -0300

    adding exporting function to vercel

--- app.js
+++ app.js
@@ -162,3 +162,5 @@ app.get("/refresh_token", function (req, res) {
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on port 3000");
 });
+
+module.exports = app;
